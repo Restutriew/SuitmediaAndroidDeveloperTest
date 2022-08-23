46071abc6b16151a2835adebe175a0551669f172
RestutriewLaptop <restutriew@gmail.com>
2022-08-23 17:01:48 +0700

    Update UI Design and Activity

--- app/src/main/java/com/restutriew/suitmediaandroiddevelopertest/GuestActivity.kt
+++ app/src/main/java/com/restutriew/suitmediaandroiddevelopertest/GuestActivity.kt
@@ -3,10 +3,9 @@ package com.restutriew.suitmediaandroiddevelopertest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class GuestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_guest)
     }
 }
